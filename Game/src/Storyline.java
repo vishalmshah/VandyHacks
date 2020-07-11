80484a10a88dc283c01ec9e9a7cf54578dbb4b54
victoryred12 <68134484+victoryred12@users.noreply.github.com>
2020-07-11 14:57:32 -0500

    Initial commit

--- Game/src/Storyline.java
+++ Game/src/Storyline.java
@@ -1,18 +1,13 @@
-// **********************************************************************************************
-// Program Name: -----
-// Name: Sianna Xu   
-// VUnetID: xuss
-// Email: sianna.s.xu@vanderbilt.edu
-// Class: CS 1101 - Vanderbilt University
-// Section: 01 MWF Arena
-// Date: --/--/--
-// Honor statement: I attest that I understand the honor code for this class and have neither given 
-//                  nor received any unauthorized aid on this assignment.
-//
-// Program Description:
-//     user types name and programs prints out a sentence with name
-//
-// **********************************************************************************************
+
+import java.util.*;
 
 public class Storyline {
+    public static void main(String[] args) {
+        Random randy = new Random(11);
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Welcome to Vanderbelt University! We're thrilled to have yo");
+
+
+    }
 }
