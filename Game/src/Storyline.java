1ac589b5b6a200c9a680e87c5d1bb073d25a4a03
victoryred12 <68134484+victoryred12@users.noreply.github.com>
2020-07-11 15:22:48 -0500

    Initial commit

--- Game/src/Storyline.java
+++ Game/src/Storyline.java
@@ -6,7 +6,31 @@ public class Storyline {
         Random randy = new Random(11);
         Scanner reader = new Scanner(System.in);
 
-        System.out.println("Welcome to Vanderbelt University! We're thrilled to have yo");
+        System.out.println("Welcome to Vanderbelt University! We're thrilled to welcome you to " +
+                "our community. Go ahead and get settled in your freshmen residential college.");
+
+        int house = randy.nextInt();
+        String houseName;
+
+        if(house == 1)
+            houseName = "North";
+        if(house == 2)
+            houseName = "West";
+        if(house == 3)
+            houseName = "East";
+        if(house == 4)
+            houseName = "Gillette";
+        if(house == 5)
+            houseName = "Hank Ingram";
+        if(house == 6)
+            houseName = "Memorial";
+        if(house == 7)
+            houseName = "Murray";
+        if(house == 8)
+            houseName = "Stambough";
+        if(house == 9)
+            houseName = "Sutherland";
+
 
 
     }
