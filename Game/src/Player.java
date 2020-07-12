77109fac6307c2ea1c58a5b65b0a99558f0635ca
Callahan-Henry <callahan.g.henry@vanderbilt.edu>
2020-07-11 23:00:05 -0400

    Most Recent #1

--- Game/src/Player.java
+++ Game/src/Player.java
@@ -16,7 +16,7 @@ public class Player {
         HP = userHP;
     }
 
-    public void setSI(int userSI) { 
+    public void setSI(int userSI) {
         SI = userSI;
     }
 
@@ -57,17 +57,5 @@ public class Player {
         SI = SI - lossOfSI;
         return HP;
     }
-
-
-    /*
-    2D player simple methods
-
-    set up like memory of location coordinates, (if they've been to light hall before, they have
-    coordinates saved)
-    when you go to location for a first time you store that location
-
-    store inventory of locations....
-
-     */
 
 }
